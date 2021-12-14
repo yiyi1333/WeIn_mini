22c77d3bfaf5949a13e56d4590de3b1c96c3d601
yiyi <80455294+yiyi1333@users.noreply.github.com>
2021-12-15 00:32:19 +0800

    完成了个人主页页面的UI设计完成个人信息卡片功能
    未授权使用微信头像和昵称的用户右边会出现一个授权按钮，授权后消失，授权完成后更新本地数据并向后台程序发送授权得到的信息更新

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-30 16:34:19
+ * @LastEditTime: 2021-12-15 00:31:04
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \WeIn\pages\my\my.js
+ */
 // pages/my/my.js
 Page({
 
@@ -5,7 +13,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    user:null,
+    userInfo:null
   },
 
   /**
@@ -26,7 +35,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    //从缓存中读取数据
+    var u = wx.getStorageSync('user');
+    this.setData({
+      user:u
+    });
+    
   },
 
   /**
@@ -62,5 +76,36 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getUserProfile(e){
+    var app = getApp();
+    wx.getUserProfile({
+      desc: '用户获取微信头像和昵称',
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+        //将获取的头像和昵称传到后台进行存储，并修改本地的头像和昵称
+        //更新本地和缓存
+        var u = this.data.user;
+        console.log(u);
+        u.consumer.avatarUrl = res.userInfo.avatarUrl;
+        u.consumer.nickName = res.userInfo.nickName;
+        this.setData({
+          user:u
+        })
+        wx.setStorageSync('user', u);
+        //给后台发送数据
+        wx.request({
+          url: app.globalData.host + 'authorize',
+          data: {
+            phoneNumber: u.consumer.phoneNumber,
+            nickName: u.consumer.nickName,
+            avatarUrl: u.consumer.avatarUrl
+          }
+        })
+      }
+    })
   }
 })
